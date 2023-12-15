a75ad3bfe5d259646bf277f8c4fb38cb9a2337c3
DanilSemanovgg1 <144227090+DanilSemanovgg1@users.noreply.github.com>
2023-12-15 15:57:08 +0300

    Add files via upload

---
+++ лаба 12 зад 3.pas
@@ -0,0 +1,13 @@
+﻿program lab12zad3;
+var
+  fileText: text;
+  S: string;
+begin
+  assign(fileText, 'kokokaka.txt');
+  append(fileText);  
+  writeln('Введите строку S:');
+  readln(S);  
+  writeln(fileText, S);
+  close(fileText);  
+  writeln('Строка добавлена в файл');  
+end.
